d39fe6f0f56e8af8ef8d9a1d8d2cbb2f2425d3bf
Tim Burgess <tim@raisedbar.net>
2018-08-25 23:12:19 +0100

    Pages for foot and panel switch configuration complete except for event handlers

--- LSAccess/GlobalsPanelSwitchesPage.cpp
+++ LSAccess/GlobalsPanelSwitchesPage.cpp
@@ -14,23 +14,22 @@ GlobalsPanelSwitchesPage::GlobalsPanelSwitchesPage(wxBookCtrlBase *parent, LinnS
 	wxBoxSizer * hBoxSettings = new wxBoxSizer(wxHORIZONTAL);
 
 	// Controls
-		/*
-		// Value is LSSwitchAssignment
-	wxRadioBox * wrbGLOBAL_SWITCH1_ASSIGN_NRPN;
-	wxRadioBox * wrbGLOBAL_SWITCH2_ASSIGN_NRPN;
+		// Value is an LSSwitchAssignment
+	wxRadioBox * wrbGLOBAL_SWITCH1_ASSIGN_NRPN = new wxRadioBox(myPanel, GLOBAL_SWITCH1_ASSIGN_NRPN_ID, L"Switch &1 assignment", wxDefaultPosition, wxDefaultSize, WXSIZEOF(wstrSwitchAssignments), wstrSwitchAssignments, m_Settings.GetGLOBAL_SWITCH1_ASSIGN_NRPN(), wxRA_SPECIFY_ROWS);
 	// Toggle:
-	wxCheckBox * chkGLOBAL_SWITCH1_BOTH_SPLITS_NRPN = new wxCheckBox(myPanel, GLOBAL_SPLIT_ACTIVE_NRPN_ID, L"Split &Active");
-	wxCheckBox * chkGLOBAL_SWITCH2_BOTH_SPLITS_NRPN = new wxCheckBox(myPanel, GLOBAL_SPLIT_ACTIVE_NRPN_ID, L"Split &Active");
+	wxCheckBox * chkGLOBAL_SWITCH1_BOTH_SPLITS_NRPN = new wxCheckBox(myPanel, GLOBAL_SWITCH1_BOTH_SPLITS_NRPN_ID, L"Switch 1 affects both splits");
 	// Values are constrained by MIN_CC and MAX_CC
-	WXSpinCtrl * wscGLOABAL_CC_FOR_CC65;  // Changes the CC for all switches - Legacy option, see NRPN 255 - 258
-	// Values are constrained by MIN_CC and MAX_CC
-	WXSpinCtrl * wscGLOBAL_CC_FOR_SWITCH1_CC65;
-	WXSpinCtrl * wscGLOBAL_CC_FOR_SWITCH2_CC65;
-	WXSpinCtrl * wscGLOBAL_CC_FOR_SWITCH1_SUSTAIN;
-	WXSpinCtrl * wscGLOBAL_CC_FOR_SWITCH2_SUSTAIN;
-	// Values are constrained by MIN_CC and MAX_CC
-	*/
-
+	// WXSpinCtrl * wscGLOABAL_CC_FOR_CC65;  // Changes the CC for all switches - Legacy option, see NRPN 255 - 258
+		wxSpinCtrl * wscGLOBAL_CC_FOR_SWITCH1_SUSTAIN = new wxSpinCtrl(myPanel, GLOBAL_CC_FOR_SWITCH1_SUSTAIN_ID, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, MIN_CC, MAX_CC, m_Settings.GetGLOBAL_CC_FOR_SWITCH1_SUSTAIN(), "CC for switch  1 sustain");
+		wxSpinCtrl * wscGLOBAL_CC_FOR_SWITCH1_CC65 = new wxSpinCtrl(myPanel, GLOBAL_CC_FOR_SWITCH1_CC65_ID, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, MIN_CC, MAX_CC, m_Settings.GetGLOBAL_CC_FOR_SWITCH1_CC65(), "CC for switch  1 CC65");
+			// Value is an LSSwitchAssignment
+	wxRadioBox * wrbGLOBAL_SWITCH2_ASSIGN_NRPN = new wxRadioBox(myPanel, GLOBAL_SWITCH2_ASSIGN_NRPN_ID, L"Switch &2 assignment", wxDefaultPosition, wxDefaultSize, WXSIZEOF(wstrSwitchAssignments), wstrSwitchAssignments, m_Settings.GetGLOBAL_SWITCH2_ASSIGN_NRPN(), wxRA_SPECIFY_ROWS);
+	// Toggle:
+	wxCheckBox * chkGLOBAL_SWITCH2_BOTH_SPLITS_NRPN = new wxCheckBox(myPanel, GLOBAL_SWITCH2_BOTH_SPLITS_NRPNN_ID, L"Switch 2 affects both splits");
+		// Values are constrained by MIN_CC and MAX_CC
+	wxSpinCtrl * wscGLOBAL_CC_FOR_SWITCH2_SUSTAIN = new wxSpinCtrl(myPanel, GLOBAL_CC_FOR_SWITCH2_SUSTAIN_ID, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, MIN_CC, MAX_CC, m_Settings.GetGLOBAL_CC_FOR_SWITCH2_SUSTAIN(), "CC for switch  2 sustain");
+	wxSpinCtrl * wscGLOBAL_CC_FOR_SWITCH2_CC65 = new wxSpinCtrl(myPanel, GLOBAL_CC_FOR_SWITCH2_CC65_ID, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, MIN_CC, MAX_CC, m_Settings.GetGLOBAL_CC_FOR_SWITCH2_CC65(), "CC for switch  2 CC65");
+	
 	myPanel->SetSizer(hBoxSettings);
 	hBoxSettings->SetSizeHints(this);
 	myPanel->Fit();
