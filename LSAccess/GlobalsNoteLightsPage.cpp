b1392c4f550fa266cf4b524e61d2f40fef856724
Tim Burgess <tim@raisedbar.net>
2018-08-28 20:06:57 +0100

    Added event handlers to GlobalsNoteLightsPage

--- LSAccess/GlobalsNoteLightsPage.cpp
+++ LSAccess/GlobalsNoteLightsPage.cpp
@@ -16,32 +16,32 @@ GlobalsNoteLightsPage::GlobalsNoteLightsPage(wxBookCtrlBase *parent, LinnStrumen
 	
 		// controls
 // Toggles:
-	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_C = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_C_ID, L"&C");
+	chkGLOBAL_MAIN_NOTE_LIGHT_C = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_C_ID, L"&C");
 	chkGLOBAL_MAIN_NOTE_LIGHT_C->SetValue(m_Settings.GetGLOBAL_MAIN_NOTE_LIGHT_C());
-	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_C_SHARP = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_C_SHARP_ID, L"C#");
+chkGLOBAL_MAIN_NOTE_LIGHT_C_SHARP = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_C_SHARP_ID, L"C#");
 	chkGLOBAL_MAIN_NOTE_LIGHT_C_SHARP->SetValue(m_Settings.GetGLOBAL_MAIN_NOTE_LIGHT_C_SHARP());
-	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_D = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_D_ID, L"&D");
+	chkGLOBAL_MAIN_NOTE_LIGHT_D = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_D_ID, L"&D");
 	chkGLOBAL_MAIN_NOTE_LIGHT_D->SetValue(m_Settings.GetGLOBAL_MAIN_NOTE_LIGHT_D());
-	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_D_SHARP = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_D_SHARP_ID, L"D#");
+	chkGLOBAL_MAIN_NOTE_LIGHT_D_SHARP = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_D_SHARP_ID, L"D#");
 	chkGLOBAL_MAIN_NOTE_LIGHT_D_SHARP->SetValue(m_Settings.GetGLOBAL_MAIN_NOTE_LIGHT_D_SHARP());
-	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_E = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_E_ID, L"&E");
+	chkGLOBAL_MAIN_NOTE_LIGHT_E = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_E_ID, L"&E");
 	chkGLOBAL_MAIN_NOTE_LIGHT_E->SetValue(m_Settings.GetGLOBAL_MAIN_NOTE_LIGHT_E());
-	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_F = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_F_ID, L"&F");
+	chkGLOBAL_MAIN_NOTE_LIGHT_F = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_F_ID, L"&F");
 	chkGLOBAL_MAIN_NOTE_LIGHT_F->SetValue(m_Settings.GetGLOBAL_MAIN_NOTE_LIGHT_F());
-	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_F_SHARP = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_F_SHARP_ID, L"F#");
+	chkGLOBAL_MAIN_NOTE_LIGHT_F_SHARP = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_F_SHARP_ID, L"F#");
 	chkGLOBAL_MAIN_NOTE_LIGHT_F_SHARP->SetValue(m_Settings.GetGLOBAL_MAIN_NOTE_LIGHT_F_SHARP());
-	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_G = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_G_ID, L"&G");
+	chkGLOBAL_MAIN_NOTE_LIGHT_G = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_G_ID, L"&G");
 	chkGLOBAL_MAIN_NOTE_LIGHT_G->SetValue(m_Settings.GetGLOBAL_MAIN_NOTE_LIGHT_G());
-	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_G_SHARP = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_G_SHARP_ID, L"G#");
+	chkGLOBAL_MAIN_NOTE_LIGHT_G_SHARP = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_G_SHARP_ID, L"G#");
 	chkGLOBAL_MAIN_NOTE_LIGHT_G_SHARP->SetValue(m_Settings.GetGLOBAL_MAIN_NOTE_LIGHT_G_SHARP());
-	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_A = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_A_ID, L"&A");
+	chkGLOBAL_MAIN_NOTE_LIGHT_A = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_A_ID, L"&A");
 	chkGLOBAL_MAIN_NOTE_LIGHT_A->SetValue(m_Settings.GetGLOBAL_MAIN_NOTE_LIGHT_A());
-	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_A_SHARP = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_A_SHARP_ID, L"A#");
+	chkGLOBAL_MAIN_NOTE_LIGHT_A_SHARP = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_A_SHARP_ID, L"A#");
 	chkGLOBAL_MAIN_NOTE_LIGHT_A_SHARP->SetValue(m_Settings.GetGLOBAL_MAIN_NOTE_LIGHT_A_SHARP());
-	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_B = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_B_ID, L"&B");
+	chkGLOBAL_MAIN_NOTE_LIGHT_B = new wxCheckBox(myPanel, GLOBAL_MAIN_NOTE_LIGHT_B_ID, L"&B");
 	chkGLOBAL_MAIN_NOTE_LIGHT_B->SetValue(m_Settings.GetGLOBAL_MAIN_NOTE_LIGHT_B());
 	// Value is constrained by MIN_LIGHTS_PRESET and MAX_LIGHTS_PRESET 
-	wxSpinCtrl * wscGLOBAL_ACTIVE_LIGHTS_PRESET = new wxSpinCtrl(myPanel, GLOBAL_ACTIVE_LIGHTS_PRESET_ID, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, MIN_LIGHTS_PRESET, MAX_LIGHTS_PRESET, m_Settings.GetGLOBAL_ACTIVE_LIGHTS_PRESET(), L"Preset");
+	wscGLOBAL_ACTIVE_LIGHTS_PRESET = new wxSpinCtrl(myPanel, GLOBAL_ACTIVE_LIGHTS_PRESET_ID, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, MIN_LIGHTS_PRESET, MAX_LIGHTS_PRESET, m_Settings.GetGLOBAL_ACTIVE_LIGHTS_PRESET(), L"Preset");
 
 	myPanel->SetSizer(hBoxSettings);
 	hBoxSettings->SetSizeHints(this);
@@ -51,3 +51,101 @@ GlobalsNoteLightsPage::GlobalsNoteLightsPage(wxBookCtrlBase *parent, LinnStrumen
 
 
 
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_C(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_MAIN_NOTE_LIGHT_C(chkGLOBAL_MAIN_NOTE_LIGHT_C->GetValue());
+}
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_C_SHARP(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_MAIN_NOTE_LIGHT_C_SHARP(chkGLOBAL_MAIN_NOTE_LIGHT_C_SHARP->GetValue());
+}
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_D(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_MAIN_NOTE_LIGHT_D(chkGLOBAL_MAIN_NOTE_LIGHT_D->GetValue());
+}
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_D_SHARP(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_MAIN_NOTE_LIGHT_D_SHARP(chkGLOBAL_MAIN_NOTE_LIGHT_D_SHARP->GetValue());
+}
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_E(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_MAIN_NOTE_LIGHT_E(chkGLOBAL_MAIN_NOTE_LIGHT_E->GetValue());
+}
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_F(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_MAIN_NOTE_LIGHT_F(chkGLOBAL_MAIN_NOTE_LIGHT_F->GetValue());
+}
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_F_SHARP(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_MAIN_NOTE_LIGHT_F_SHARP(chkGLOBAL_MAIN_NOTE_LIGHT_F_SHARP->GetValue());
+}
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_G(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_MAIN_NOTE_LIGHT_G(chkGLOBAL_MAIN_NOTE_LIGHT_G->GetValue());
+}
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_G_SHARP(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_MAIN_NOTE_LIGHT_G_SHARP(chkGLOBAL_MAIN_NOTE_LIGHT_G_SHARP->GetValue());
+}
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_A(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_MAIN_NOTE_LIGHT_A(chkGLOBAL_MAIN_NOTE_LIGHT_A->GetValue());
+}
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_A_SHARP(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_MAIN_NOTE_LIGHT_A_SHARP(chkGLOBAL_MAIN_NOTE_LIGHT_A_SHARP->GetValue());
+}
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_B(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_MAIN_NOTE_LIGHT_B(chkGLOBAL_MAIN_NOTE_LIGHT_B->GetValue());
+}
+
+
+void GlobalsNoteLightsPage::OnGLOBAL_ACTIVE_LIGHTS_PRESET(wxSpinEvent& event)
+{
+	m_Settings.SetGLOBAL_ACTIVE_LIGHTS_PRESET(wscGLOBAL_ACTIVE_LIGHTS_PRESET->GetValue());
+}
+
+
+// Event table
+BEGIN_EVENT_TABLE(GlobalsNoteLightsPage, wxPanel)
+EVT_CHECKBOX(GLOBAL_MAIN_NOTE_LIGHT_C_ID, GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_C)
+EVT_CHECKBOX(GLOBAL_MAIN_NOTE_LIGHT_C_SHARP_ID, GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_C_SHARP)
+EVT_CHECKBOX(GLOBAL_MAIN_NOTE_LIGHT_D_ID, GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_D)
+EVT_CHECKBOX(GLOBAL_MAIN_NOTE_LIGHT_D_SHARP_ID, GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_D_SHARP)
+EVT_CHECKBOX(GLOBAL_MAIN_NOTE_LIGHT_E_ID, GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_E)
+EVT_CHECKBOX(GLOBAL_MAIN_NOTE_LIGHT_F_ID, GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_F)
+EVT_CHECKBOX(GLOBAL_MAIN_NOTE_LIGHT_F_SHARP_ID, GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_F_SHARP)
+EVT_CHECKBOX(GLOBAL_MAIN_NOTE_LIGHT_G_ID, GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_G)
+EVT_CHECKBOX(GLOBAL_MAIN_NOTE_LIGHT_G_SHARP_ID, GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_G_SHARP)
+
+EVT_CHECKBOX(GLOBAL_MAIN_NOTE_LIGHT_A_ID, GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_A)
+EVT_CHECKBOX(GLOBAL_MAIN_NOTE_LIGHT_A_SHARP_ID, GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_A_SHARP)
+EVT_CHECKBOX(GLOBAL_MAIN_NOTE_LIGHT_B_ID, GlobalsNoteLightsPage::OnGLOBAL_MAIN_NOTE_LIGHT_B)
+
+EVT_SPINCTRL(GLOBAL_ACTIVE_LIGHTS_PRESET_ID, GlobalsNoteLightsPage::OnGLOBAL_ACTIVE_LIGHTS_PRESET)
+END_EVENT_TABLE()
--- LSAccess/GlobalsNoteLightsPage.h
+++ LSAccess/GlobalsNoteLightsPage.h
@@ -27,8 +27,42 @@ public:
 	GlobalsNoteLightsPage(wxBookCtrlBase *parent, LinnStrument * pLinnStrument);
 
 private:
+	// event handlers
+	DECLARE_EVENT_TABLE()
+
+	void OnGLOBAL_MAIN_NOTE_LIGHT_C(wxCommandEvent& event);
+	void OnGLOBAL_MAIN_NOTE_LIGHT_C_SHARP(wxCommandEvent& event);
+	void OnGLOBAL_MAIN_NOTE_LIGHT_D(wxCommandEvent& event);
+	void OnGLOBAL_MAIN_NOTE_LIGHT_D_SHARP(wxCommandEvent& event);
+	void OnGLOBAL_MAIN_NOTE_LIGHT_E(wxCommandEvent& event);
+	void OnGLOBAL_MAIN_NOTE_LIGHT_F(wxCommandEvent& event);
+	void OnGLOBAL_MAIN_NOTE_LIGHT_F_SHARP(wxCommandEvent& event);
+	void OnGLOBAL_MAIN_NOTE_LIGHT_G(wxCommandEvent& event);
+	void OnGLOBAL_MAIN_NOTE_LIGHT_G_SHARP(wxCommandEvent& event);
+	void OnGLOBAL_MAIN_NOTE_LIGHT_A(wxCommandEvent& event);
+	void OnGLOBAL_MAIN_NOTE_LIGHT_A_SHARP(wxCommandEvent& event);
+	void OnGLOBAL_MAIN_NOTE_LIGHT_B(wxCommandEvent& event);
+	void OnGLOBAL_ACTIVE_LIGHTS_PRESET(wxSpinEvent& event);
+	
+	// data
 	LinnStrument * pMyLinnStrument;
 	wxBookCtrlBase *pMyParent;
 	LSGlobalSettings m_Settings;
+
+	// controls
+	// Toggles:
+	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_C;
+	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_C_SHARP;
+	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_D;
+	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_D_SHARP;
+	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_E;
+	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_F;
+	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_F_SHARP;
+	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_G;
+	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_G_SHARP;
+	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_A;
+	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_A_SHARP;
+	wxCheckBox * chkGLOBAL_MAIN_NOTE_LIGHT_B;
+	wxSpinCtrl * wscGLOBAL_ACTIVE_LIGHTS_PRESET;
 };
 
