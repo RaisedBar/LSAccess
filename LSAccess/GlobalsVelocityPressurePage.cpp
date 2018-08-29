efc0175b7ecf8da9300517749bbb5e5b17c5f050
Tim Burgess <tim@raisedbar.net>
2018-08-29 16:15:49 +0100

    Added event handlers to GlobalsVelocityPressurePage

--- LSAccess/GlobalsVelocityPressurePage.cpp
+++ LSAccess/GlobalsVelocityPressurePage.cpp
@@ -15,11 +15,11 @@ GlobalsVelocityPressurePage::GlobalsVelocityPressurePage(wxBookCtrlBase *parent,
 
 	// Controls
 				// Value is an LSVelocityRange
-	wxRadioBox * wrbGLOBAL_VELOCITY_SENSITIVITY = new wxRadioBox(myPanel, GLOBAL_VELOCITY_SENSITIVITY_ID, L"&Velocity sensitivity", wxDefaultPosition, wxDefaultSize, WXSIZEOF(VelocityRanges), VelocityRanges, m_Settings.GetGLOBAL_VELOCITY_SENSITIVITY(), wxRA_SPECIFY_ROWS);
+	wrbGLOBAL_VELOCITY_SENSITIVITY = new wxRadioBox(myPanel, GLOBAL_VELOCITY_SENSITIVITY_ID, L"&Velocity sensitivity", wxDefaultPosition, wxDefaultSize, WXSIZEOF(VelocityRanges), VelocityRanges, m_Settings.GetGLOBAL_VELOCITY_SENSITIVITY(), wxRA_SPECIFY_ROWS);
 	// Values are constrained by MIN_CC and MAX_CC
-	wxSpinCtrl * wscGLOBAL_MIN_VELOCITY_VALUE = new wxSpinCtrl(myPanel, GLOBAL_MIN_VELOCITY_VALUE_ID, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, MIN_CC, MAX_CC, m_Settings.GetGLOBAL_MIN_VELOCITY_VALUE(), L"Min. velocity");
-	wxSpinCtrl * wscGLOBAL_MAX_VELOCITY_VALUE = new wxSpinCtrl(myPanel, GLOBAL_MAX_VELOCITY_VALUE_ID, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, MIN_CC, MAX_CC, m_Settings.GetGLOBAL_MAX_VELOCITY_VALUE(), L"Max. velocity");
-	wxSpinCtrl * wscGLOBAL_FIXED_VELOCITY_VALUE = new wxSpinCtrl(myPanel, GLOBAL_FIXED_VELOCITY_VALUE_ID, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, MIN_CC, MAX_CC, m_Settings.GetGLOBAL_FIXED_VELOCITY_VALUE(), L"&Tempo");
+	wscGLOBAL_MIN_VELOCITY_VALUE = new wxSpinCtrl(myPanel, GLOBAL_MIN_VELOCITY_VALUE_ID, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, MIN_CC, MAX_CC, m_Settings.GetGLOBAL_MIN_VELOCITY_VALUE(), L"Min. velocity");
+	wscGLOBAL_MAX_VELOCITY_VALUE = new wxSpinCtrl(myPanel, GLOBAL_MAX_VELOCITY_VALUE_ID, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, MIN_CC, MAX_CC, m_Settings.GetGLOBAL_MAX_VELOCITY_VALUE(), L"Max. velocity");
+	wscGLOBAL_FIXED_VELOCITY_VALUE = new wxSpinCtrl(myPanel, GLOBAL_FIXED_VELOCITY_VALUE_ID, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, MIN_CC, MAX_CC, m_Settings.GetGLOBAL_FIXED_VELOCITY_VALUE(), L"&Tempo");
 	// Value is an LSPressureRange
 	wxRadioBox * wrbGLOBAL_PRESSURE_SENSITIVITY = new wxRadioBox(myPanel, GLOBAL_PRESSURE_SENSITIVITY_ID, L"&Pressure sensitivity", wxDefaultPosition, wxDefaultSize, WXSIZEOF(PressureRanges), PressureRanges, m_Settings.GetGLOBAL_PRESSURE_SENSITIVITY(), wxRA_SPECIFY_ROWS);
 	// Toggle
@@ -33,3 +33,52 @@ GlobalsVelocityPressurePage::GlobalsVelocityPressurePage(wxBookCtrlBase *parent,
 }
 
 
+// Event handlers
+
+void GlobalsVelocityPressurePage::OnGLOBAL_VELOCITY_SENSITIVITY(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_VELOCITY_SENSITIVITY(wrbGLOBAL_VELOCITY_SENSITIVITY->GetSelection());
+}
+
+
+void GlobalsVelocityPressurePage::OnGLOBAL_PRESSURE_AFTERTOUCH(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_PRESSURE_AFTERTOUCH(chkGLOBAL_PRESSURE_AFTERTOUCH->GetValue());
+}
+
+
+void GlobalsVelocityPressurePage::OnGLOBAL_PRESSURE_SENSITIVITY(wxCommandEvent& event)
+{
+	m_Settings.SetGLOBAL_PRESSURE_SENSITIVITY(wrbGLOBAL_PRESSURE_SENSITIVITY->GetSelection());
+}
+
+
+void GlobalsVelocityPressurePage::OnGLOBAL_FIXED_VELOCITY_VALUE(wxSpinEvent& event)
+{
+	m_Settings.SetGLOBAL_FIXED_VELOCITY_VALUE(wscGLOBAL_FIXED_VELOCITY_VALUE->GetValue());
+}
+
+
+void GlobalsVelocityPressurePage::OnGLOBAL_MAX_VELOCITY_VALUE(wxSpinEvent& event)
+{
+	m_Settings.SetGLOBAL_MAX_VELOCITY_VALUE(wscGLOBAL_MAX_VELOCITY_VALUE->GetValue());
+}
+
+
+void GlobalsVelocityPressurePage::OnGLOBAL_MIN_VELOCITY_VALUE(wxSpinEvent& event)
+{
+	m_Settings.SetGLOBAL_MIN_VELOCITY_VALUE(wscGLOBAL_MIN_VELOCITY_VALUE->GetValue());
+}
+
+
+// Event table
+BEGIN_EVENT_TABLE(GlobalsVelocityPressurePage, wxPanel)
+EVT_SPINCTRL(GLOBAL_MIN_VELOCITY_VALUE_ID, GlobalsVelocityPressurePage::OnGLOBAL_MIN_VELOCITY_VALUE)
+EVT_SPINCTRL(GLOBAL_MAX_VELOCITY_VALUE_ID, GlobalsVelocityPressurePage::OnGLOBAL_MAX_VELOCITY_VALUE)
+EVT_SPINCTRL(GLOBAL_FIXED_VELOCITY_VALUE_ID, GlobalsVelocityPressurePage::OnGLOBAL_FIXED_VELOCITY_VALUE)
+
+EVT_RADIOBOX(GLOBAL_VELOCITY_SENSITIVITY_ID, GlobalsVelocityPressurePage::OnGLOBAL_PRESSURE_SENSITIVITY)
+EVT_RADIOBOX(GLOBAL_PRESSURE_SENSITIVITY_ID, GlobalsVelocityPressurePage::OnGLOBAL_PRESSURE_SENSITIVITY)
+
+EVT_CHECKBOX(GLOBAL_PRESSURE_AFTERTOUCH_ID, GlobalsVelocityPressurePage::OnGLOBAL_PRESSURE_AFTERTOUCH)
+END_EVENT_TABLE()
--- LSAccess/GlobalsVelocityPressurePage.h
+++ LSAccess/GlobalsVelocityPressurePage.h
@@ -27,8 +27,31 @@ public:
 	GlobalsVelocityPressurePage(wxBookCtrlBase *parent, LinnStrument * pLinnStrument);
 
 private:
+	// event handlers
+	DECLARE_EVENT_TABLE()
+
+	void OnGLOBAL_VELOCITY_SENSITIVITY(wxCommandEvent& event);
+	void OnGLOBAL_PRESSURE_AFTERTOUCH(wxCommandEvent& event);
+	void OnGLOBAL_PRESSURE_SENSITIVITY(wxCommandEvent& event);
+void OnGLOBAL_FIXED_VELOCITY_VALUE(wxSpinEvent& event);
+	void OnGLOBAL_MAX_VELOCITY_VALUE(wxSpinEvent& event);
+void OnGLOBAL_MIN_VELOCITY_VALUE(wxSpinEvent& event);
+	
+// data
 	LinnStrument * pMyLinnStrument;
 	wxBookCtrlBase *pMyParent;
 	LSGlobalSettings m_Settings;
-};
+
+// controls
+				// Value is an LSVelocityRange
+	wxRadioBox * wrbGLOBAL_VELOCITY_SENSITIVITY;
+	// Values are constrained by MIN_CC and MAX_CC
+	wxSpinCtrl * wscGLOBAL_MIN_VELOCITY_VALUE;
+	wxSpinCtrl * wscGLOBAL_MAX_VELOCITY_VALUE;
+	wxSpinCtrl * wscGLOBAL_FIXED_VELOCITY_VALUE;
+	// Value is an LSPressureRange
+	wxRadioBox * wrbGLOBAL_PRESSURE_SENSITIVITY;
+	// Toggle
+	wxCheckBox * chkGLOBAL_PRESSURE_AFTERTOUCH;
+	};
 
